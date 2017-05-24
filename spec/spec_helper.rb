a81b296ba005de556090306e8d0aaaf128099633
HuyAnh <huyanhhb@yahoo.com.vn>
2017-05-24 20:31:49 +0700

    xin chao

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,8 @@ require 'rspec'
 require 'capybara'
 require 'capybara/dsl'
 require 'capybara/poltergeist'
-
+require 'rails_helper'
+require 'spec_helper'
 RSpec.configure do |config|
   config.include Capybara::DSL
 end
